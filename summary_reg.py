b1414778cd8ebb8045c66f17fa400a21c5dd82f3
ikaros <mariano.cabezas@udg.edu>
2026-02-11 16:29:23 +0100

    Bug fixes

--- summary_reg.py
+++ summary_reg.py
@@ -336,6 +336,17 @@ def deformable_registration(path, data_dict, scales, epochs, patience, lr):
                 scales=scales, epochs=epochs, patience=patience
             )
 
+            moved = resample(
+                bl_im, bl_nii.header.get_zooms(),
+                bl_im.shape, bl_nii.header.get_zooms(),
+                torch.eye(4), df
+            ).detach().cpu().numpy()
+
+            moved_nii = nib.Nifti1Image(moved, None, header=out_hdr)
+            moved_nii.to_filename(
+                os.path.join(path, 'Basal_IronMET_CGM', c, 'sT1W_3D_TFE_SENSE_warped.nii.gz')
+            )
+
             df_numpy = np.moveaxis(df.detach().cpu().numpy(), 0,-1)
             df_nii = nib.Nifti1Image(df_numpy, None, header=out_hdr)
             df_nii.to_filename(
