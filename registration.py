605fcf2280bd0c235a67735dbec6b533b99ca9b5
ikaros <mariano.cabezas@udg.edu>
2026-02-11 12:34:55 +0100

    Bug fixes

--- registration.py
+++ registration.py
@@ -644,6 +644,7 @@ def nonlinear_registration(
             tensor_moved = moved.view((1, 1) + moved.shape).to(device)
             tensor_moved3 = conv3(tensor_moved)
             tensor_moved5 = conv5(tensor_moved)
+            print(moved.shape, tensor_moved.shape, fixed_norm.shape)
             tensor_fixed = fixed_norm.view((1, 1) + fixed_norm.shape).to(device)
             tensor_fixed3 = conv3(tensor_fixed)
             tensor_fixed5 = conv5(tensor_fixed)
