4fd28873fb94c0abf3ed17ff6bf2fba083819dd7
ikaros <mariano.cabezas@udg.edu>
2026-03-26 12:52:19 +0100

    Added jacobian determinant

--- summary_reg.py
+++ summary_reg.py
@@ -709,19 +709,19 @@ def main():
 
             h_atrophy = atrophy_data['healthy_natrophy']
             h_matrophy = np.mean(h_atrophy)
-            if h_atrophy > 0:
+            if h_matrophy > 0:
                 h_atrophy_s = '\033[32m{:>5.2f}%\033[0m'.format(h_matrophy)
             else:
                 h_atrophy_s = '\033[31m{:>5.2f}%\033[0m'.format(h_matrophy)
             o_atrophy = np.mean(atrophy_data['obese_natrophy'])
             o_matrophy = np.mean(h_atrophy)
-            if o_atrophy > 0:
+            if o_matrophy > 0:
                 o_atrophy_s = '\033[32m{:>5.2f}%\033[0m'.format(o_matrophy)
             else:
                 o_atrophy_s = '\033[31m{:>5.2f}%\033[0m'.format(o_matrophy)
             s_atrophy = np.mean(atrophy_data['surgery_natrophy'])
             s_matrophy = np.mean(h_atrophy)
-            if s_atrophy > 0:
+            if s_matrophy > 0:
                 s_atrophy_s = '\033[32m{:>5.2f}%\033[0m'.format(s_matrophy)
             else:
                 s_atrophy_s = '\033[31m{:>5.2f}%\033[0m'.format(s_matrophy)
